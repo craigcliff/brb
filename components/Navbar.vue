d79418775438109ab149960fe4619c2a2558d4cb
craigcliff <craigcliff@gmail.com>
2017-12-11 17:55:10 +0200

    'flex-nav'

--- components/Navbar.vue
+++ components/Navbar.vue
@@ -1,22 +1,82 @@
 <template>
-  <div class="container">
-   <nav class="navbar" role="navigation" aria-label="main navigation">
-     <div class="navbar-brand">
-        <a class="navbar-item" href="/">Portfolio</a>
-        <button class="button navbar-burger">
-          <span></span>
-          <span></span>
-          <span></span>
-        </button>
-      </div>
-      <div class="navbar-menu">
-        <div class="navbar-end">
-          <nuxt-link class="navbar-item" to="/">Home</nuxt-link>
-          <nuxt-link class="navbar-item" to="/about">About Me</nuxt-link>
-          <nuxt-link class="navbar-item" to="/projects">Projects</nuxt-link>
-          <nuxt-link class="navbar-item" to="/contact">Contact</nuxt-link>
+<div class="navigation">
+            
+   
+                      
+
+            <nav  class="navigation__nav"  >
+                <ul  class="navigation__list"  >
+
+                 
+                  <li class="navigation__item"><nuxt-link class="navigation__link"  to="/">Home</nuxt-link></li>
+                  <li class="navigation__item"><nuxt-link class="navigation__link"  to="/about">About</nuxt-link></li>
+                  <li class="navigation__item"><nuxt-link class="navigation__link" to="/projects">Projects</nuxt-link></li>
+                  <li class="navigation__item"><nuxt-link class="navigation__link"  to="/contact">Contact</nuxt-link></li>
+                   
+    
+                    </ul>
+            </nav>
         </div>
-      </div>
-    </nav>
-  </div>
+
 </template>
+
+<script>
+export default{
+  data () {
+    return {
+      showNav: false
+    }
+  }
+}
+</script>
+
+<style lang="scss">
+
+.navigation{
+
+   max-width: 1000px;
+  margin: 0 auto;
+  width: 100%;
+
+  &__nav {
+    
+       
+         position: fixed;
+    width: 100%;
+        z-index: 1500;
+    
+    
+        transition: all .8s cubic-bezier(0.68, -0.55, 0.265, 1.55);
+    }
+
+    &__list{
+ display: -webkit-box;
+  display: -moz-box;
+  display: -ms-flexbox;
+  display: -webkit-flex;
+  display: flex;
+ -webkit-flex-flow: row ;
+  -moz-flex-flow: row ;
+  -ms-flex-flow: row ;
+  flex-flow: row;
+  -webkit-justify-content: center;
+  -moz-justify-content: center;
+  justify-content: center;
+  align-content: flex-end; 
+}
+
+    
+    &__item{
+
+      -webkit-box-flex: 0 1 50%;
+  -moz-box-flex: 0 1 50%;
+  -webkit-flex: 0 1 50%;
+  -ms-flex: 0 1 50%;
+  flex: 0 1 50%;
+
+padding-left: 15px;
+    }
+}
+
+
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,9 +1,35 @@
 <template>
-  <section class="section is-large">
-    <div class="container has-text-centered">
-      <h1 class="title">Hi, I'm Craig Hector</h1>
-      <h2 class="subtitle">A web Developer based in Cape Town, South Africa.</h2>
-    </div>
-  </section>
+<header class="header">
+
+
+ </header>
 </template>
 
+
+<style lang="scss">
+
+
+.header {
+    height: 100Vh;
+    
+       
+    background-image: linear-gradient(
+        to right bottom,
+        rgba(0, 255, 0, 0.8),
+        rgba(0, 0, 255, 0.8));
+    background-size: cover;
+    background-position: top;
+    position: relative;
+
+    @media only screen and (min-resolution: 192dpi) and (min-width: 37.5em),
+            only screen and (-webkit-min-device-pixel-ratio: 2) and (min-width: 37.5em),
+            only screen and (min-width: 125em) {
+        background-image: linear-gradient(
+            to right bottom,
+            rgba(0, 255, 0, 0.8),
+        rgba(0, 0, 255, 0.8));
+}
+
+}
+
+</style>
