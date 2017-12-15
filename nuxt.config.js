511822ff0aabec9fd88494272d8b4f99be2dacb2
craig <craigcliff@gmail.com>
2017-12-15 15:52:14 +0200

    optimized images

--- nuxt.config.js
+++ nuxt.config.js
@@ -33,7 +33,7 @@ module.exports = {
         ],
         link: [
             { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
-            { rel: 'stylesheet', href: 'https://cdnjs.cloudflare.com/ajax/libs/bulma/0.6.0/css/bulma.min.css' },
+           
             { rel: 'stylesheet', href: 'https://fonts.googleapis.com/css?family=Source+Code+Pro:400,700' }
         ]
     },
